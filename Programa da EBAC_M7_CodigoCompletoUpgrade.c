09bc94b81f8a3e74ae328f6ac2bd347026cf518c
devrenanoliveira <124644015+devrenanoliveira@users.noreply.github.com>
2023-02-08 21:28:41 -0300

    Opção de senha de administrador
    Acrescentada a opção de acesso ao menu principal a partir de uma senha pré-determinada no código.

--- Programa da EBAC_M7_CodigoCompletoUpgrade.c
+++ Programa da EBAC_M7_CodigoCompletoUpgrade.c
@@ -177,51 +177,68 @@ int main()
 	{
 	int opcao = 0;
 	int laco=1;
+	char senha[10]="a";
+	int comparacao;
 	
-	for(laco=1;laco=1;) //tradução: faça quantoas vezes eu pedir -> a variável LACO enquanto for valor 1 (entrada), ele deve ficar no "for" até 1x o menu (saída) e sempre subindo 1 (x++)
+	setlocale(LC_ALL, "Portuguese"); //definição do local como português para poder colocar os ascentos	
+	
+	printf(" ### Bem-vinda(o) ao Cartório da EBAC ### \n\n"); //início do menu
+	printf("Login de administrador.\n\nDigite a sua senha: ");
+	scanf("%s",senha);
+	
+	comparacao = strcmp(senha, "admin"); //strcmp é COMPARAÇÃO DE STRINGS. Se a comparação for igual, o programa vai salvar um 0 (zero) na variavel "comparacao"
+	
+	if(comparacao == 0)
 	{
-		
-		system("cls"); //limpo a tela de opções para deixar o programa mais amigável e organizado para o usuário
-		
-		setlocale(LC_ALL, "Portuguese"); //definição do local como português para poder colocar os ascentos
-		
-		printf(" ### Bem-vinda(o) ao Cartório da EBAC ### \n\n"); //início do menu
-		printf("Escolha a opção desejada do menu: \n\n");
-		printf("\t1 - Registrar nomes\n");
-		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n");
-		printf("\t4 - Sair do sistema\n\n");
-		printf("\nOpção: "); //fim do menu
-	
-		scanf ("%d", &opcao); //armazenando a escolha do usuário
-	
-		system("cls"); //limpo a tela de opções para deixar o programa mais amigável e organizado para o usuário
-	
-	
-		switch (opcao) //início da seleção
-		{
-			case 1: //caso a variável tenha valor 1 ele tem que fazer a instrução abaixo
-			registro(); //chamando a função REGISTRO criada no início do código, antes da função principal MAIN
-			break;
-			
-			case 2:	//caso a variável tenha valor 2 ele tem que fazer a instrução abaixo
-			consulta(); //chamando a função CONSULTA criada no início do código, antes da função principal MAIN
-			break;
-			
-			case 3: //caso a variável tenha valor 3 ele tem que fazer a instrução abaixo
-			deletar(); //chamando a função DELETAR criada no início do código, antes da função principal MAIN
-			break;
-			
-			case 4:
-			printf("Obrigado por utilizar o sistema!\n");
-			return 0; //é o função para sair do sistema
-			break;
 
-			default: //caso a variável tenha valor diferente de 1, 2 ou 3, ele tem que fazer a instrução abaixo
-			printf("Opção inválida! Você será redirecionado ao menu inicial para escolher uma opção válida. \n\n");
-			system("pause");
-			break;
-		}
+	
+		for(laco=1;laco=1;) //tradução: faça quantoas vezes eu pedir -> a variável LACO enquanto for valor 1 (entrada), ele deve ficar no "for" até 1x o menu (saída) e sempre subindo 1 (x++)
+		{
 		
+			system("cls"); //limpo a tela de opções para deixar o programa mais amigável e organizado para o usuário
+			
+			printf("\n ### Bem-vinda(o) ao Cartório da EBAC ### \n\n"); //início do menu		
+			printf("Escolha a opção desejada do menu: \n\n");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar nomes\n");
+			printf("\t3 - Deletar nomes\n");
+			printf("\t4 - Sair do sistema\n\n");
+			printf("\nOpção: "); //fim do menu
+	
+			scanf ("%d", &opcao); //armazenando a escolha do usuário
+	
+			system("cls"); //limpo a tela de opções para deixar o programa mais amigável e organizado para o usuário
+	
+	
+			switch (opcao) //início da seleção
+			{
+				case 1: //caso a variável tenha valor 1 ele tem que fazer a instrução abaixo
+				registro(); //chamando a função REGISTRO criada no início do código, antes da função principal MAIN
+				break;
+			
+				case 2:	//caso a variável tenha valor 2 ele tem que fazer a instrução abaixo
+				consulta(); //chamando a função CONSULTA criada no início do código, antes da função principal MAIN
+				break;
+			
+				case 3: //caso a variável tenha valor 3 ele tem que fazer a instrução abaixo
+				deletar(); //chamando a função DELETAR criada no início do código, antes da função principal MAIN
+				break;
+			
+				case 4:
+				printf("Obrigado por utilizar o sistema!\n");
+				return 0; //é o função para sair do sistema
+				break;
+
+				default: //caso a variável tenha valor diferente de 1, 2 ou 3, ele tem que fazer a instrução abaixo
+				printf("Opção inválida! Você será redirecionado ao menu inicial para escolher uma opção válida. \n\n");
+				system("pause");
+				break;
+			}
+		}
 	}
+	
+	else
+		printf("Senha incorreta! Tente novamente.\n\n");
+		system("pause");
+		return main();
 }
